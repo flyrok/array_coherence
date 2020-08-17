22317abd942769996348e4f59b06f85d814d3c7c
flyrok@gmail.com <flyrok@gmail.com>
2020-08-16 21:49:16 -0400

    mv'd version info here

--- array_coherence/__init__.py
+++ array_coherence/__init__.py
@@ -2,3 +2,9 @@
 from array_coherence.array_coherence import array_coherence
 from array_coherence.template_config import template_config
 from array_coherence.plot_coherence import plot_twosta
+from array_coherence.setup_log import setup_log
+
+progname='array_coherence';
+__version_info__ = ('2020','08','16')
+__version__ = '.'.join(__version_info__)
+
